8a6d9c4faf05d559a0852996365f0f7020d53bba
shinyfuwante <mr.philliptang@gmail.com>
2023-12-22 00:16:50 -0800

    Test method to send to backend

--- src/components/DraftTool/DraftTool.tsx
+++ src/components/DraftTool/DraftTool.tsx
@@ -1,13 +1,20 @@
 import { Component, createEffect, onMount } from "solid-js";
-import {w3cwebsocket as WebSocket} from "websocket";
+import { w3cwebsocket as WebSocket } from "websocket";
 import { useParams } from "solid-start";
 
 const DraftTool: Component<{}> = (props) => {
-    const params = useParams();
-    const game_id = params.game_id;
+  const params = useParams();
+  const game_id = params.game_id;
   const backendUrl = `ws://localhost:8000/ws/game/${game_id}`;
   const client = new WebSocket(backendUrl);
-
+  const sendTest = () => {
+    console.log('Sending test');
+    const testObj = {
+      game_state: "test",
+      type: "testing",
+    };
+    client.send(JSON.stringify(testObj));
+  };
   onMount(async () => {
     client.onopen = () => {
       console.log("WebSocket Client Connected");
@@ -16,7 +23,12 @@ const DraftTool: Component<{}> = (props) => {
       console.log(message);
     };
   });
-  return <div>Connection: </div>;
+  return (
+    <div>
+      <div>Connection:</div>
+      <button onClick={() => sendTest()}>Send Test</button>
+    </div>
+  );
 };
 
 export default DraftTool;
