ec607bf8ea323f50753eddeb5562e5a933772635
shinyfuwante <mr.philliptang@gmail.com>
2023-12-28 01:37:06 -0800

    Set up game_logic to process messages

--- src/components/DraftTool/DraftTool.tsx
+++ src/components/DraftTool/DraftTool.tsx
@@ -1,12 +1,21 @@
 import { Component, createEffect, onMount } from "solid-js";
 import { w3cwebsocket as WebSocket } from "websocket";
 import { useParams } from "solid-start";
+import { handleMsg } from "~/game/game_logic";
 
 const DraftTool: Component<{}> = (props) => {
   const params = useParams();
   const game_id = params.game_id;
   const backendUrl = `ws://localhost:8000/ws/game/${game_id}`;
   const client = new WebSocket(backendUrl);
+  const LoadingMenu = () => {
+    return (
+      <div>
+        Send this link to your friend to join the game:
+        <a href={window.location.href}>{window.location.href}</a>
+      </div>
+    );
+  }
   const sendTest = () => {
     console.log('Sending test');
     const testObj = {
@@ -20,12 +29,13 @@ const DraftTool: Component<{}> = (props) => {
       console.log("WebSocket Client Connected");
     };
     client.onmessage = (message: any) => {
-      console.log(message);
+      handleMsg(message.data);
     };
   });
   return (
     <div>
       <div>Connection:</div>
+      <LoadingMenu></LoadingMenu>
       <button onClick={() => sendTest()}>Send Test</button>
     </div>
   );
