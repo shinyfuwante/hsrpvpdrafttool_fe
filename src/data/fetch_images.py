d3e8da5a35f8c3613020e64516daa11b4a6b0da2
shinyfuwante <mr.philliptang@gmail.com>
2023-12-29 20:33:04 -0800

    Add icons, playerboy and stuff

--- src/data/fetch_images.py
+++ src/data/fetch_images.py
@@ -5,15 +5,31 @@ from PIL import Image
 # Load the JSON file into a dictionary
 with open('./StarRailRes/index_min/en/characters.json', 'r') as f:
     characters = json.load(f)
+    
+def convert_and_move_images(src_dir, dest_dir, file_ext):
+    for filename in os.listdir(src_dir):
+        if filename.endswith(file_ext):
+            # Get the ID and name of the character
+            id = filename.split('.')[0]
+            if id == '8000' or 'None': 
+                continue;
+            name = characters[id]['name']
+            if name == '{NICKNAME}':
+                name = characters[id]['tag']
 
-# Iterate through all the images
-for filename in os.listdir('./StarRailRes/image/character_preview'):
-    if filename.endswith('.png'):
-        # Get the ID and name of the character
-        id = filename.split('.')[0]
-        name = characters[id]['name']
+            # Check if the destination file already exists
+            dest_file = f'{dest_dir}/{name}.webp'
+            if os.path.exists(dest_file):
+                print(f'Skipping {dest_file}, already exists')
+                continue
 
-        # Open the image file
-        img = Image.open(f'./StarRailRes/image/character_preview/{filename}')
-        # Convert and save the image to the new directory with the new name
-        img.save(f'../../public/character_images/{name}.webp', 'webp')
+            # Open the image file
+            img = Image.open(f'{src_dir}/{filename}')
+            # Convert and save the image to the new directory with the new name
+            img.save(dest_file, 'webp')
+            
+# Convert and move character preview images
+convert_and_move_images('./StarRailRes/image/character_preview', '../../public/character_images', '.png')
+
+# Convert and move character icon images
+convert_and_move_images('./StarRailRes/icon/character', '../../public/character_icons', '.png')
