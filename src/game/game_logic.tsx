adced0629ce600a58da04399729d41ff0d886a52
shinyfuwante <mr.philliptang@gmail.com>
2024-01-06 18:11:45 -0800

    Clean up game_logic

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createEffect } from "solid-js";
+import { createSignal} from "solid-js";
 import { CharJsonType, LCJsonType } from "~/types";
 const game_phases = {
   LOADING: "loading",
