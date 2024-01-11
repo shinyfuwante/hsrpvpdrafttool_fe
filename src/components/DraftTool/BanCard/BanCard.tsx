bed99eb8ed7546cd72112ec19da65df27dcec6d5
shinyfuwante <mr.philliptang@gmail.com>
2024-01-11 02:03:07 -0800

    Abstract styles to BanCard.module.css

--- src/components/DraftTool/BanCard/BanCard.tsx
+++ src/components/DraftTool/BanCard/BanCard.tsx
@@ -1,6 +1,7 @@
 import { Component} from "solid-js";
 import { charJson } from "~/game/game_logic";
 import { CharacterBan } from "~/game/game_logic";
+import styles from "./BanCard.module.css";
 
 interface BanCardProps {
   character: CharacterBan;
@@ -9,16 +10,9 @@ export const BanCard: Component<BanCardProps> = ({ character }) => {
   const char = charJson()[character.name];
   return (
     <div
+      class={styles.ban_card}
       style={{
         "background-image": `url(/character_images/${char.id}.webp)`,
-        "background-color": "#D3D3D3",
-        "max-width": "100%",
-        height: "50%",
-        "background-size": "cover",
-        "background-position": "30% 30%",
-        filter: "grayscale(100%)",
-        "min-height": "125px",
-        border: "1px solid grey",
       }}
     ></div>
   );
