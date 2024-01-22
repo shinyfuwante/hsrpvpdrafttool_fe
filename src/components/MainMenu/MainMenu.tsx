e261a8771a3e4b02148c4a2cc8fe5dcbe2c583f7
shinyfuwante <mr.philliptang@gmail.com>
2024-01-21 18:02:36 -0800

    Prototyping the look of main menu

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -12,10 +12,34 @@ const MainMenu: Component<MainMenuProps> = (props: any) => {
     const url = `/game/${game_id}`;
     window.location.href = url;
   };
+  const soloGame = () => {
+    const url = `/solo/game`;
+    window.location.href = url;
+  };
   return (
     <div>
       {props.children}
-      <button onClick={() => joinGame()}>Create Game</button>
+      <div 
+      style = {{
+        "display": "flex",
+        "flex-direction": "row",
+        "justify-content": "space-evenly",
+        "align-content": "center",
+      }}>
+        <div>
+          <div>
+            To create a game for Screen-Share drafting (e.g. Discord, etc):
+          </div>
+          <button onClick={() => soloGame()}>Create Solo Draft Game</button>
+        </div>
+        <div>
+          <div>To create a game for multiplayer lobby-style drafting:</div>
+          <button onClick={() => joinGame()}>Create Lobby Draft Game</button>
+          <div>
+            (Note: the player who selects side will be randomly selected.)
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
