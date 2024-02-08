9ff65e8c055c2b46099400d29a8b8f0862569494
shinyfuwante <mr.philliptang@gmail.com>
2024-02-07 21:21:30 -0800

    Stylize solo settings some

--- src/components/SoloSettings/SoloSettings.tsx
+++ src/components/SoloSettings/SoloSettings.tsx
@@ -32,7 +32,7 @@ const SoloSettings: Component<{}> = (props) => {
       setRedTeamName(name);
     }
     setPlayer2Name(name);
-  }
+  };
   const determineBlueTeam = (name: string) => {
     if (player1Name() === name) {
       setBlueTeamName(player1Name());
@@ -45,61 +45,63 @@ const SoloSettings: Component<{}> = (props) => {
 
   return (
     <div class={styles.settings_menu}>
-        <h3>Settings: </h3>
+      <h3>Settings: </h3>
       <div>
-        <div>
-          <input
-            type="text"
-            placeholder="Team 1 Name"
-            onChange={(e) => updatePlayer1Name(e.target.value)}
-          ></input>
+        <div class={styles.team_names}>
+          <div>
+            <input
+              type="text"
+              placeholder="Team 1 Name"
+              onChange={(e) => updatePlayer1Name(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Team 2 Name"
+              onChange={(e) => updatePlayer2Name(e.target.value)}
+            ></input>
+          </div>
         </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Team 2 Name"
-            onChange={(e) => updatePlayer2Name(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <button onClick={() => setPlayer1Roll(roll())}>
+        <div class={styles.initiative}>
+          <div
+            class={styles.roll_buttons}
+            onClick={() => setPlayer1Roll(roll())}
+          >
+            <div>{player1Roll()}</div>
             Roll for {player1Name()}
-          </button>
-        </div>
-        <div>
-          <button onClick={() => setPlayer2Roll(roll())}>
+          </div>
+          <div
+            class={styles.roll_buttons}
+            onClick={() => setPlayer2Roll(roll())}
+          >
+            <div>{player2Roll()}</div>
             Roll for {player2Name()}
-          </button>
+          </div>
         </div>
       </div>
       <div>
-        <div>
-          <div>Team 1 Name: {player1Name()}</div>
-          <div>Team 2 Name: {player2Name()}</div>
-        </div>
-        <div>
-          <div>Team 1 Roll: {player1Roll()}</div>
-          <div>Team 2 Roll: {player2Roll()}</div>
-        </div>
-        <div>
-          Blue Team is:
-          <input
-            type="radio"
-            id="player1"
-            name="team"
-            value={player1Name()}
-            checked={true}
-            onChange={(e) => determineBlueTeam(player1Name())}
-          />
-          <label for="player1">{player1Name()}</label>
-          <input
-            type="radio"
-            id="player2"
-            name="team"
-            value={player2Name()}
-            onChange={(e) => determineBlueTeam(player2Name())}
-          />
-          <label for="player2">{player2Name()}</label>
+        <div class={styles.blue_team_selector}>
+          Blue Team:
+          <div>
+            <input
+              type="radio"
+              id="player1"
+              name="team"
+              value={player1Name()}
+              checked={true}
+              onChange={(e) => determineBlueTeam(player1Name())}
+            />
+            <label for="player1">{player1Name()}</label>
+            <input
+              type="radio"
+              id="player2"
+              name="team"
+              value={player2Name()}
+              onChange={(e) => determineBlueTeam(player2Name())}
+            />
+            <label for="player2">{player2Name()}</label>
+          </div>
         </div>
       </div>
     </div>
