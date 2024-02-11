85924fbc5a8e3649086f51e3a758c89f5b611c9b
shinyfuwante <mr.philliptang@gmail.com>
2024-02-10 16:18:17 -0800

    Fix .env variable on front end

--- src/routes/game/[game_id].tsx
+++ src/routes/game/[game_id].tsx
@@ -19,7 +19,7 @@ export default function GamePage() {
   const params = useParams();
   const game_id = params.game_id;
   const ruleSetString = ruleSet();
-  const backendUrl = `wss://hsrpvpdrafttoolbe-production.up.railway.app:8000/ws/game/${game_id}?ruleSet=${ruleSetString}&cid=${getCID()}`;
+  const backendUrl = `wss://${import.meta.env.VITE_BACKEND_URL}/ws/game/${game_id}?ruleSet=${ruleSetString}&cid=${getCID()}`;
   const client = new WebSocket(backendUrl);
   const LoadingMenu = () => {
     return (
