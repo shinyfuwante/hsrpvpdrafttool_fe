f16cc15cef16e7b3696fdca49db7022061614c7a
shinyfuwante <mr.philliptang@gmail.com>
2024-02-18 16:46:36 -0800

    Add click to copy instead of click to redirect, to slightly improve robustness of websocket connection

--- src/routes/game/[game_id].tsx
+++ src/routes/game/[game_id].tsx
@@ -1,5 +1,5 @@
 import { useParams } from "solid-start";
-import { onMount, Show } from "solid-js";
+import { onMount, Show, createSignal } from "solid-js";
 import {
   ruleSet,
   sideSelector,
@@ -19,29 +19,60 @@ export default function GamePage() {
   const params = useParams();
   const game_id = params.game_id;
   const ruleSetString = ruleSet();
-  const backendUrl = `wss://${import.meta.env.VITE_BACKEND_URL}/ws/game/${game_id}?ruleSet=${ruleSetString}&cid=${getCID()}`;
+  const backendUrl = `wss://${
+    import.meta.env.VITE_BACKEND_URL
+  }/ws/game/${game_id}?ruleSet=${ruleSetString}&cid=${getCID()}`;
   const client = new WebSocket(backendUrl);
+  const [copied, setCopied] = createSignal(false);
+  const copiedModal = () => {
+    return (
+      <div class={styles.copied_modal}>
+        <div>Link Copied!</div>
+      </div>
+    );
+  };
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      setCopied(true);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
   const LoadingMenu = () => {
     return (
-      <div class={styles.loading_screen}>
-        <div class={styles.loading_message}>
-          <Show when={error() != ""}>{error()}</Show>
-          <div>
-            Send this link to your friend to join the game: 
-            <a href={window.location.href}>{window.location.href}</a>
+      <div class={styles.loading_container}>
+        <div class={styles.loading_screen}>
+          <div class={styles.loading_message}>
+            <Show when={error() != ""}>{error()}</Show>
+            <div>
+              Waiting for another player. Click to copy this link to send to
+              your friend:
+              {/* <a href={window.location.href}>{window.location.href}</a> */}
+              <div onClick={copyToClipboard} class={styles.link_to_copy}>
+                {window.location.href}
+              </div>
+            </div>
           </div>
         </div>
+        <Show when={copied()}>{copiedModal()}</Show>
       </div>
     );
   };
   const ReconnectScreen = () => {
     return (
-      <div class={styles.loading_screen}>
-        <div class={styles.loading_message}>
-          Other player has disconnected. Send this link to your friend to join
-          the game:
-          <a href={window.location.href}>{window.location.href}</a>
+      <div class={styles.loading_container}>
+        <div class={styles.loading_screen}>
+          <div class={styles.loading_message}>
+            Other player has disconnected. Send this link to your friend to join
+            the game:
+            {/* <a href={window.location.href}>{window.location.href}</a> */}
+            <div onClick={copyToClipboard} class={styles.link_to_copy}>
+              {window.location.href}
+            </div>
+          </div>
         </div>
+        <Show when={copied()}>{copiedModal()}</Show>
       </div>
     );
   };
@@ -100,14 +131,26 @@ export default function GamePage() {
       <div>
         {sideSelector() && (
           <div class={styles.side_selector}>
-            <button class={styles.blue_team} onClick={() => sendSideMessage("blue")}>Blue Team</button>
-            <button class={styles.red_team} onClick={() => sendSideMessage("red")}>Red Team</button>
+            <button
+              class={styles.blue_team}
+              onClick={() => sendSideMessage("blue")}
+            >
+              Blue Team
+            </button>
+            <button
+              class={styles.red_team}
+              onClick={() => sendSideMessage("red")}
+            >
+              Red Team
+            </button>
           </div>
         )}
         {!sideSelector() && (
           <div class={styles.loading_screen}>
-            <div class={styles.loading_message}>Waiting for other player to select side...</div>
+            <div class={styles.loading_message}>
+              Waiting for other player to select side...
             </div>
+          </div>
         )}
       </div>
     );
