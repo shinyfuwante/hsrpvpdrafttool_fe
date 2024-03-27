51aa4244e5cbf572355d7604e85ccf5b522cf3dd
shinyfuwante <mr.philliptang@gmail.com>
2024-03-26 22:30:42 -0700

    Finalize 2.1 release changes

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 
 export const version = () => {
   // version of tool.version of game.subversion of game.subversion of rules
-  return "1.2.1.0.0";
+  return "1.2.1.0.1";
 }
 
 const POINTS_PER_SUPERIMPOSITION = 0.5;
