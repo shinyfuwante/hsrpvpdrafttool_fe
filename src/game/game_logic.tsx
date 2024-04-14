6492719182f39b8933f1a5820674fe81cca51106
shinyfuwante <mr.philliptang@gmail.com>
2024-04-14 11:36:05 -0700

    1.2.1.1

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 
 export const version = () => {
   // version of tool.version of game.subversion of game.subversion of rules
-  return "1.2.1.0.3";
+  return "1.2.1.1.0";
 }
 
 const POINTS_PER_SUPERIMPOSITION = 0.5;
