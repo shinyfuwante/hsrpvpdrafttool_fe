149504289113b5e982a0808c87d1f2a867457194
shinyfuwante <mr.philliptang@gmail.com>
2024-04-19 12:52:28 -0700

    Cache busting

--- src/root.tsx
+++ src/root.tsx
@@ -40,7 +40,7 @@ export default function Root() {
         <Title>Ditto's Star Rail PVP Draft Tool</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/ico" href="/favicon.ico?v=1.0.0"/>
+        <link rel="shortcut icon" type="image/ico" href="/favicon.ico?v=1.0.1"/>
       </Head>
       <Body>
         <Suspense>
