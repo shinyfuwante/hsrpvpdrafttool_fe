8a0cf3ef8ce573432fde36bdec52845502984425
shinyfuwante <mr.philliptang@gmail.com>
2024-05-31 01:59:16 -0700

    fix hanya e6

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 
 export const version = () => {
   // version of tool.version of game.subversion of game.subversion of rules
-  return "2.2.2.1.2"; 
+  return "2.2.2.1.3"; 
 }
 
 const POINTS_PER_SUPERIMPOSITION = 0.5;
