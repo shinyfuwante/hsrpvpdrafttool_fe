0ee14b784b2b6c16b34df0c2103d7b700477ae98
shinyfuwante <mr.philliptang@gmail.com>
2024-06-21 18:25:54 -0700

    Tourney

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 
 export const version = () => {
   // version of tool.version of game.subversion of game.subversion of rules
-  return "2.2.3.0.0"; 
+  return "2.2.3.0.1"; 
 }
 
 const POINTS_PER_SUPERIMPOSITION = 0.5;
