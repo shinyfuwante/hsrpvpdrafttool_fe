e6dfcf339fff5693177206357b139f9d13c4358e
shinyfuwante <mr.philliptang@gmail.com>
2024-08-17 13:31:37 -0700

    add costs route

---
+++ src/routes/solo/costs.tsx
@@ -0,0 +1,13 @@
+import { Component, createEffect, createSignal } from "solid-js";
+import PhdTable from "~/components/PhdTable/PhdTable";
+
+const costs: Component<{}> = (props) => {
+  
+  return (
+    <div>
+        <PhdTable></PhdTable>
+    </div>
+  );
+};
+
+export default costs;
