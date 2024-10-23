c66a2a004772302808d66faab45b02562c1babe8
shinyfuwante <mr.philliptang@gmail.com>
2024-10-23 16:11:04 -0700

    2.6

--- src/components/DraftTool/BanCard/BanCard.tsx
+++ src/components/DraftTool/BanCard/BanCard.tsx
@@ -1,6 +1,6 @@
 import { Component} from "solid-js";
 import { charJson } from "~/game/game_logic";
-import { CharacterBan, turnOrder, turn_order_3_bans } from "~/game/game_logic";
+import { CharacterBan, turnOrder, turn_order_2_bans } from "~/game/game_logic";
 import styles from "./BanCard.module.css";
 
 interface BanCardProps {
@@ -8,9 +8,10 @@ interface BanCardProps {
 }
 export const BanCard: Component<BanCardProps> = ({ character }) => {
   const char = charJson()[character.name];
+  const style = turnOrder() == turn_order_2_bans ? styles.ban_card : styles.ban_row_half;
   return (
     <div
-      class={`${turnOrder() == turn_order_3_bans ? styles.ban_row_half : styles.ban_card}`}
+      class={`${style}`}
       style={{
         "background-image": `url(/character_images/${char.id}.webp)`,
       }}
