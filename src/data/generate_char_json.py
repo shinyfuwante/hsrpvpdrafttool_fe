e88bb537d15022ad36fcfdadcdd0a80d4aaa9700
shinyfuwante <mr.philliptang@gmail.com>
2025-01-15 15:01:46 -0800

    Some minor script changes

--- src/data/generate_char_json.py
+++ src/data/generate_char_json.py
@@ -6,7 +6,7 @@ def process_items(items, new_data_file):
     # Load the new JSON file, if it exists
     new_data = {}
     if os.path.exists(new_data_file):
-        with open(new_data_file, 'r') as f:
+        with open(new_data_file, 'r', encoding='utf-8') as f:
             new_data = json.load(f)
 
     # Iterate through all the items
@@ -28,15 +28,25 @@ def process_items(items, new_data_file):
 
     # Write the new dictionary to a new JSON file
     with open(new_data_file, 'w') as f:
-        json.dump(new_data, f, indent=4)
+        json.dump(new_data, f, indent=4, ensure_ascii=False)
 
 # Load the existing JSON files into dictionaries
-with open('./StarRailRes/index_min/en/characters.json', 'r') as f:
+with open('./StarRailRes/index_min/cn/characters.json', 'r', encoding='utf-8') as f:
     characters = json.load(f)
+    
+with open('./StarRailRes/index_min/en/characters.json', 'r', encoding='utf-8') as f:
+    en_characters = json.load(f)
 
-with open('./StarRailRes/index_min/en/light_cones.json', 'r') as f:
+with open('./StarRailRes/index_min/cn/light_cones.json', 'r', encoding='utf-8') as f:
     lightcones = json.load(f)
+    
+with open('./StarRailRes/index_min/en/light_cones.json', 'r', encoding='utf-8') as f:
+    en_lightcones = json.load(f)
 
 # Process the characters and lightcones
-process_items(characters, './characters.json')
-process_items(lightcones, './light_cones.json')
+process_items(characters, './characters_cn.json')
+process_items(en_characters, './characters.json')
+process_items(lightcones, './light_cones_cn.json')
+process_items(en_lightcones, './light_cones.json')
+
+print("done")
