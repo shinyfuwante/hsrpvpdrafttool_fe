2c5348adb4de03f2b6580d41a1b356d47908f17b
shinyfuwante <mr.philliptang@gmail.com>
2025-01-24 20:55:20 -0800

    cache bust

--- src/game/game_logic.tsx
+++ src/game/game_logic.tsx
@@ -4,7 +4,7 @@ import { v4 } from "uuid";
 
 export const version = () => {
   // version of tool.version of game.subversion of game.subversion of rules
-  return "3.3.0.2";
+  return "3.3.0.3";
 };
 
 const POINTS_PER_SUPERIMPOSITION = 0.5;
